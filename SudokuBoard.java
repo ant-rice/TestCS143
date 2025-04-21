771a44ed03a727d43d07b2c9e13df605605bdffb
ant-rice <antonio_rice@icloud.com>
2025-04-20 20:08:11 -0700

    finished reading from file and assigning to board

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,10 +1,30 @@
+import java.util.*;
+import java.io.*;
+
 public class SudokuBoard {
-   private int[][] board;
+   private char[][] board;
    
-   public SudokuBoard(String filename) {
+   public SudokuBoard(String filename) throws FileNotFoundException {
+      board = new char[9][9];
+      Scanner readBoard = new Scanner(new File(filename));
+      char val;
+      
+      for(int i = 0; i < 9; i++) { // going through each line
+         String line = readBoard.nextLine();
+         for(int j = 0; j < line.length(); j++) {
+            if (line.charAt(j) == '.') {
+               val = '_';
+            } else {
+               val = line.charAt(j);
+            } 
+            board[i][j] = val;
+            
+         }
+      }
    }
    
    public String toString() {
-      return "";
+      String result = "";
+      return result;
    }
 }
